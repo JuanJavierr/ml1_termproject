ff5f3eccdd4e5b987064cf9d9f30d18c7b01c8c5
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-03-27 20:09:11 -0400

    up modules

--- modules/train_rnn.py
+++ modules/train_rnn.py
@@ -25,7 +25,7 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     
-    nltk.download('punkt')
+    #nltk.download('punkt')
     #nltk.download('stopwords')
     nlp = spacy.load("fr_core_news_sm")
 
