ca75f5a7ba130c009d7938ab345fe007abd80893
JuanJavierr <xavierpeladeau12@hotmail.com>
2024-03-29 20:45:20 +0000

    bugfix

--- modules/utils.py
+++ modules/utils.py
@@ -17,6 +17,9 @@ def build_dataset(path, num_samples=-1, rnd_state=42):
     df3 = pd.read_json(path + "/mars.json")
     df4 = pd.read_json(path + "/decembre.json")
     df = pd.concat([df1, df2, df3, df4], ignore_index=True)
+
+    df = cleanup_dataset(df)
+
     df["section_label"], _ = pd.factorize(df["section_1"])
     if num_samples != -1:
         df = df.sample(
